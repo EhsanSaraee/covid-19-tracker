7259acba1dacbf4314611c4955e5e9c3d47cd609
ehsan <ehsansaraee6570@yahoo.com>
2022-03-26 17:35:08 +0430

    adding country picker component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import { Cards, Chart, CountryPicker } from './Components';
 import styles from './App.module.css';
 import { useEffect, useState } from 'react';
 import { fetchData } from './Api';
+import logo from './images/image.png';
 
 const App = () => {
    const [data, setData] = useState({});
+   const [country, setCountry] = useState('');
+
+   const handleCountryChange = async (country) => {
+      const data = await fetchData(country);
+      setCountry(data);
+   };
 
    useEffect(() => {
       fetchData().then((data) => setData(data));
@@ -12,9 +19,10 @@ const App = () => {
 
    return (
       <section className={styles.container}>
+         <img className={styles.image} src={logo} alt="COVID-19" />
          <Cards data={data} />
-         <CountryPicker />
-         <Chart />
+         <CountryPicker handleCountryChange={handleCountryChange} />
+         <Chart data={data} country={country} />
       </section>
    );
 };
--- src/Components/CountryPicker/CountryPicker.jsx
+++ src/Components/CountryPicker/CountryPicker.jsx
@@ -1,10 +1,33 @@
-import './CountryPicker.module.css';
+import { FormControl, NativeSelect } from '@material-ui/core';
+import { useEffect, useState } from 'react';
+import { fetchCountries } from '../../Api';
+import styles from './CountryPicker.module.css';
 
-const CountryPicker = () => {
+const CountryPicker = ({ handleCountryChange }) => {
+   const [countries, setCountries] = useState([]);
+
+   useEffect(() => {
+      const fetchAPI = async () => {
+         setCountries(await fetchCountries());
+      };
+
+      fetchAPI();
+   }, []);
+
    return (
-      <>
-         <h1>CountryPicker</h1>
-      </>
+      <FormControl className={styles.formControl}>
+         <NativeSelect
+            defaultValue=""
+            onChange={(e) => handleCountryChange(e.target.value)}
+         >
+            <option value="">United States</option>
+            {countries.map((country, i) => (
+               <option key={i} value={country}>
+                  {country}
+               </option>
+            ))}
+         </NativeSelect>
+      </FormControl>
    );
 };
 
